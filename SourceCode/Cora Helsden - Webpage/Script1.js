88d6be214230b9e8514644aa67e2970f5d78311d
Cora Helsden <146185185+Coraa404@users.noreply.github.com>
2026-01-01 17:25:24 +0000

    Added Register page
    - Register Page added
    - Fixed some Login page styling
    - Added framework for guest login
    - Added titles to each new HTML doc

--- SourceCode/Cora Helsden - Webpage/Script1.js
+++ SourceCode/Cora Helsden - Webpage/Script1.js
@@ -6,6 +6,8 @@ let contentPage = document.getElementById('content')
 function changePage(page) {
     switch (page) {
         case 'home':
+            document.title = "Welcome! | Crossing Danger Analysis"
+
             contentPage.innerHTML = `
                 <div class="WelcomeCard">
                     <h1>Crossing Danger Analysis System</h1>
@@ -20,37 +22,77 @@ function changePage(page) {
         break;
 
         case 'login':
+            document.title = "Login | Crossing Danger Analysis"
+
             contentPage.innerHTML = `
                 <div class="LoginCard">
                     <h1>Login</h1>
 
                     <form id="loginDetails">
-                        <label for="usernameInput"> Username: / Email: </label>
+                        <label for="usernameInput"> Username / Email: </label>
                         <input type="text" id="usernameInput" placeholder="Enter Username or Email">
 
                         <label for="passwordInput">Password:</label>
                         <input type="password" id="passwordInput" placeholder="Enter password">
 
-                        <button type="submit" class="btn" id="loginBtn">Submit</button>
+                        <button type="submit" class="btn" id="loginBtn">Continue</button>
                         <p class="smallBtn" id="back">Back</p>
                     </form>
                 </div>
             `;
-        break;
+            break;
+
+        case 'register':
+            document.title = "Register | Crossing Danger Analysis"
+
+            contentPage.innerHTML = `
+                <div class="RegisterCard">
+                    <h1>Register</h1>
+
+                    <form id="registerDetails">
+                        <label for="usernameInputReg"> Username: </label>
+                        <input type="text" id="usernameInputReg" placeholder="Enter Username.">
+
+                        <label for="emailInputReg"> Email: </label>
+                        <input type="text" id="emailInputReg" placeholder="Enter Email.">
+
+                        <label for="passwordInputReg"> Password: </label>
+                        <input type="password" id="passwordInputReg" placeholder="Enter Password.">
+
+                        <label for="confirmPasswordInputReg"> Confirm Password: </label>
+                        <input type="password" id="confirmPasswordInputReg" placeholder="Enter Password Again.">
+
+                        <button type="submit" class="btn" id="registerBtn">Continue</button>
+                        <p class="smallBtn" id="back">Back</p>
+                    </form>
+                </div>
+            `;
+            break;
 
     }
 
     const loginButton = document.getElementById("LoginButton");
     const returnButton = document.getElementById("back");
+    const registerButton = document.getElementById("RegisterButton");
+
+    const guestButton = document.getElementById("GuestButton");
 
     if (loginButton) {
         loginButton.addEventListener("click", () => {
             changePage("login");
         });
-    } else if (returnButton) {
+    }
+
+    if (registerButton) {
+        registerButton.addEventListener("click", () => {
+            changePage("register")
+        });
+    }
+
+    if (returnButton) {
         returnButton.addEventListener("click", () => {
             changePage("home")
-        })
+        });
     }
 }
 
